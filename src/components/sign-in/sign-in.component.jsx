3b012a6270618543a9d46a40ffad297bf3181a62
Eduardo Martins <mirassol.code@gmail.com>
2021-01-07 17:35:52 -0300

    Fix the email and password fields flashing on Google sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -58,7 +58,7 @@ class SignIn extends React.Component {
                  required />
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignin >
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignin >
               Sign in with Google
             </CustomButton>
           </div>
