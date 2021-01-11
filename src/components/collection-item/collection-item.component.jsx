cfb663113469d476ffeb75b18af5f762ba80a030
Eduardo Martins <mirassol.code@gmail.com>
2021-01-11 11:05:43 -0300

    Updated cart add item to group items by quantity, and added a new version of items data

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions'
+
 import './collection-item.styles.scss'
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+import CustomButton from '../custom-button/custom-button.component'
+
+const CollectionItem = ({ item, addItem }) => {
+  const {id, name, price, imageUrl} = item;
+  return (
   <div key={id} className="collection-item">
     <div className="image"
          style={ {
@@ -13,7 +20,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
          <span className="name">{name}</span>
          <span className="price">{price}</span>
     </div>
+    <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
   </div> 
-)
+)}
 
-export default CollectionItem
+const mapDispatchToPtops = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToPtops)(CollectionItem)
