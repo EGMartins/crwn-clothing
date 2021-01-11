d4df5dcd3a3fbc16d3dccec1f8d9c10a63bbe0e8
Eduardo Martins <mirassol.code@gmail.com>
2021-01-11 16:14:02 -0300

    Added addItem and removeItem functionality to the checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+
+import { connect } from 'react-redux'
+
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
+
 import './checkout-item.styles.scss'
 
-const CheckouItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+const CheckouItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return(
   <div className="checkout-item">
     <div className="image-container">
       <img src={imageUrl} alt="Item"/>
     </div>
     <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
+    <span className="quantity">
+    <div onClick={() => removeItem(cartItem)} className="arrow">&#10094;</div>
+      <span className="value">{quantity}</span>  
+    <div onClick={() => addItem(cartItem)} className="arrow">&#10095;</div>
+    </span>
     <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
+    <span onClick={() => clearItem(cartItem)} className="remove-button">&#10005;</span>
   </div>
-)
+)}
 
-export default CheckouItem
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckouItem)
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
@@ -17,6 +17,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
       };
     default:
       return state;
